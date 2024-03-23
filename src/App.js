1515fe1aa613f45de4c746e64bd8becf07c56907
Mostafa Mohamed Mostafa <54216066+mostafamohamedmostafa@users.noreply.github.com>
2024-03-23 11:09:44 +0200

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -1,44 +1,23 @@
-
 import { useEffect } from "react";
 import AOS from "aos";
 import "aos/dist/aos.css";
 import Nav from "./components/Nav";
-import Hero from "./components/Hero";
-import Monechip from "./components/Monechip";
-import Cpu from "./components/Cpu";
-import Gpu from "./components/Gpu";
-import Battery from "./components/Battery";
-import Machinelearning from "./components/Machinelearning";
-import Macos from "./components/Macos";
-import Retinadisplayintro from "./components/Retinadisplay"
-import Cameraintro from "./components/Camera";
-import MagicKeyboardintro from "./components/MagicKeyboard";
-import Connectivityintro from "./components/Connectivity";
-import Ar from "./components/Ar";
-function App() {
+import { Link, Route, Routes } from "react-router-dom";
 
+import OldVersionPage from "./components/OldVersionPage";
+import NewVersionPage from "./components/NewVersionPage";
+function App() {
   useEffect(() => {
     AOS.init();
     AOS.refresh();
   }, []);
   return (
-
-
     <div className="App">
-      
-      <Hero />
-      <Monechip />
-      <Cpu />
-      <Gpu />
-      <Battery />
-      <Machinelearning />
-      <Macos />
-      <Retinadisplayintro />
-      <Cameraintro />
-      <MagicKeyboardintro />
-      <Connectivityintro />
-      <Ar/>
-
+      <Nav />
+      <Routes>
+        <Route path="OldVersionPage" element={<OldVersionPage />} />
+        <Route path="NewVersionPage" element={<NewVersionPage />} />
+      </Routes>
     </div>
   );
 }
